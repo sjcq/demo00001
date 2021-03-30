5ad4f8d07d7c400dab362228e71be20863fe8959
stw <ziyistw7y@163.com>
2021-03-30 18:16:21 +0800

    1

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.atguigu;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("nihaohao");
+        System.out.println(new Date());
     }
 }
