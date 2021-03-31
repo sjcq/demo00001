334f948e1f608a43f96f68efb3d5f3f6bf1a8c15
stw <ziyistw7y@163.com>
2021-03-31 09:13:13 +0800

    1

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -6,5 +6,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("nihaohao");
         System.out.println(new Date());
+
+
+        System.out.println("jintiande");
     }
 }
